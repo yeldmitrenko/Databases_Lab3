6e9cdc4d020b9f52b65ecfaaad3cde567e1e72e8
Yelizaveta Dmitrenko <74879089+yeldmitrenko@users.noreply.github.com>
2021-11-23 23:14:59 +0300

    edited create_table procedure

--- procedures/create_tables.sql
+++ procedures/create_tables.sql
@@ -1,32 +1,27 @@
 DROP PROCEDURE IF EXISTS create_tables;
+
 DELIMITER //
 CREATE PROCEDURE create_tables()
 BEGIN
-	DECLARE done BOOL DEFAULT FALSE;
-	DECLARE new_name VARCHAR(45);
-	DECLARE city_cursor CURSOR FOR SELECT name FROM city;
+	DECLARE done INT DEFAULT FALSE;
+	DECLARE name VARCHAR(45);
+	DECLARE city_cursor CURSOR FOR SELECT city.name FROM cite;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND
     SET done = TRUE;
 OPEN city_cursor;
 city_loop: LOOP
-	FETCH city_cursor INTO new_name;
-    IF done THEN LEAVE city_loop;
-    END IF;
-    SET @city_db := CONCAT('CREATE DATABASE IF NOT EXISTS ', new_name, ';');
-    PREPARE my_query from @city_db;
-		EXECUTE my_query;
-    SET @table_count := 1;
-
-	WHILE @table_count < rand() * 4 
-    DO
-		SET @new_city_table = CONCAT('CREATE TABLE IF NOT EXISTS', new_name, '.', new_name, @table_count, '(id INT, name VARCHAR(45));');
-		SELECT @new_city_table;
-		PREPARE table_query FROM @new_city_table;
-		EXECUTE my_query;
-		SET @table_count = @table_count + 1;
-		END WHILE;
-        
-	END LOOP;
-	CLOSE city_cursor;
-END //
-DELIMITER ;
+   FETCH city_cursor INTO name;
+   IF done = TRUE THEN LEAVE city_loop;
+   END IF;
+   SET @table_count = 1;
+while_loop: WHILE @table_count < 5 DO
+    SET @new_table = CONCAT('CREATE TABLE IF NOT EXISTS ', name, '(id INT, name VARCHAR(45));');
+    SELECT @new_table;
+    PREPARE myquery FROM @new_table;
+    EXECUTE myquery;
+    SET @table_count = @table_count + 1;
+   END WHILE;
+  END LOOP;
+  CLOSE city_cursor;
+END ;
+//
