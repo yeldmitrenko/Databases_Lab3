464fe9ddaf3c4c9cde8058a6e31a6d8333ee7b9d
Yelizaveta Dmitrenko <74879089+yeldmitrenko@users.noreply.github.com>
2021-11-26 00:08:25 +0300

    added update for student table

--- insert_into_db.sql
+++ insert_into_db.sql
@@ -19,18 +19,26 @@ INSERT INTO student_group (name, group_number, entry_year) VALUES
 	('AAA', '11', '2020'),
     ('BBB', '12', '2019'),
     ('CCC', '13', '2020');
+
+INSERT INTO academic_debt (name, student_marked_debt_id) VALUES 
+	('economics', '1'), 
+    ('geography', '2'),
+    ('journalism', '3');
+
     
-INSERT INTO student (name, surname, overall_rating, birthday_date, entry_date, student_number, email, city_id, completed_secondary_education_id, student_group_id, student_marked_debt_id) VALUES 
-	('Polina', 'Kovalchyck', '35', '2002-09-23', '2020-08-23', '12345', 'polina@gmail.com', '1', '2', '1', '1'),
+INSERT INTO student (name, surname, overall_rating, birthday_date, entry_date, student_number, email, city_id,
+	completed_secondary_education_id, student_group_id, student_marked_debt_id) VALUES 
+	('Polina222', 'Kovalchyck', '35', '2002-09-23', '2020-08-23', '12345', 'polina@gmail.com', '1', '2', '1', '1'),
     ('Oleg', 'Krivich', '27', '2002-06-12', '2019-05-18', '435234', 'oleg@gmail.com', '1', '2', '1', '2'),
-	('Max', 'Lyachuv', '16', '2003-01-19', '2020-09-01', '550678', 'max@gmail.com', '1', '2', '1', '3');
+	('Max', 'Lyachuv', '16', '2003-01-19', '2020-09-01', '890678', 'max@gmail.com', '1', '2', '1', '3');
+    
+UPDATE student
+SET name='HH', surname='Kovalchyck', overall_rating = "35", birthday_date = "2002-09-23", entry_date = '2020-08-23', student_number = '12345', email = 'polina@gmail.com', city_id = "1",
+	completed_secondary_education_id = "2", student_group_id = "1", student_marked_debt_id = "1"
+WHERE id = 1;
+
     
 INSERT INTO student_marked_debt (student_id, academic_debt_id) VALUES
 	('1', '1'),
     ('2', '2'),
     ('3', '3');
-    
-INSERT INTO academic_debt (name, student_marked_debt_id) VALUES 
-	('economics', '1'), 
-    ('geography', '2'),
-    ('journalism', '3');
